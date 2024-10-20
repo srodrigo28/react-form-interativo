8874e84cb0d953697ede5f053960a6f31404febe
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2024-10-20 16:34:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### React TypeScript e Validações com Form Interativo.
+* Mask Input
+* Form Control
+* Fetch Cep
+
+##### Ref.
+```
+https://www.youtube.com/watch?v=H2_yqSFCy4g
+```
---
+++ src/components/FormHeader.tsx
@@ -0,0 +1,8 @@
+export default function FormHeader() {
+  return (
+    <header className="mb-8">
+      <h1 className="text-slate-100 font-semibold text-3xl">Cadastre-se</h1>
+      <p className="text-slate-100">Crie sua conta para começar.</p>
+    </header>
+  );
+}
